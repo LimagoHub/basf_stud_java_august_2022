af30c326ff3cea0f3350438e89270787bdbb0474
Jo Wagner <jowagner@limago.de>
2022-08-11 11:46:05 +0200

    Initial commit

---
+++ Tag4_02BouncingBall/src/de/games/BouncingBall.java
@@ -0,0 +1,62 @@
+package de.games;
+
+import java.awt.Color;
+import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+
+public class BouncingBall extends Frame{
+
+	private BufferStrategy strategy;
+	private boolean gameover = false;
+	
+	private int x,y;
+	private int xRichtung, yRichtung;
+	
+	public BouncingBall() {
+		x =  y = 400;
+		xRichtung = 2;
+		yRichtung = 1;
+		
+		setSize(800, 800);
+		setVisible(true);
+		this.createBufferStrategy(2);
+		strategy = this.getBufferStrategy();
+	}
+	
+	public static void main(String[] args) {
+		new BouncingBall().run();
+
+	}
+	
+	public void run() {
+		while(! gameover) {
+			calculateScene();
+			renderScene();
+			
+		}
+	}
+
+	private void calculateScene() {
+		x += xRichtung;
+		y += yRichtung;
+		
+		if( x < 0 || x > getWidth())
+			xRichtung *= -1;
+		
+		if( y < 0 || y > getHeight())
+			yRichtung *= -1;
+		
+	}
+
+	private void renderScene() {
+		Graphics g = strategy.getDrawGraphics();
+		g.setColor(Color.RED);
+		g.clearRect(0, 0, getWidth(), getHeight());
+		g.fillOval(x-10, y-10, 20, 20);
+		g.dispose();
+		strategy.show();
+		
+	}
+
+}
