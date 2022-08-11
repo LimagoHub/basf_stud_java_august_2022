53a7d4764ad65c7998b1848040ec7d1d116145a2
Jo Wagner <jowagner@limago.de>
2022-08-11 11:45:49 +0200

    Initial commit

--- Tag4_01PaintClone/src/de/basf/gui/PaintClone.java
+++ Tag4_01PaintClone/src/de/basf/gui/PaintClone.java
@@ -2,19 +2,23 @@ package de.basf.gui;
 
 import java.awt.Color;
 import java.awt.Frame;
+import java.awt.Graphics;
 import java.awt.MenuBar;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.awt.image.BufferedImage;
 
 public class PaintClone extends Frame {
 
 	
 	public enum Formen {LINIE, OVAL,RECHTECK,LOESCHEN};
-	
 	private static final int SIZE = 800;
 	private Color color = Color.WHITE;
 	private Formen form = Formen.LINIE;
 	
+	private BufferedImage image = new BufferedImage(SIZE, SIZE, BufferedImage.TYPE_INT_RGB);
 	
 	
 	public Formen getForm() {
@@ -40,13 +44,44 @@ public class PaintClone extends Frame {
 				dispose();
 			}
 		});
+		addMouseListener(new MyMouseListener());
 		setSize(SIZE, SIZE);
 		setResizable(false);
 		setMenuBar(new MyMenubar(this));
 	}
 	
+	@Override
+	public void paint(Graphics g) {
+		g.drawImage(image, 0,0, SIZE,SIZE, this);
+	}
+	
 	public static void main(String[] args) {
 		new PaintClone().setVisible(true);
+	}
+	
+	class MyMouseListener extends MouseAdapter {
+		private int x,y;
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+			x = e.getX();
+			y = e.getY();
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			Graphics g = image.getGraphics();
+			g.setColor(getColor());
+			switch(getForm()) {
+			case LINIE: g.drawLine(x, y, e.getX(), e.getY()); break;
+			case OVAL: g.drawOval(x, y, e.getX()-x, e.getY()-y); break;
+			case RECHTECK: g.drawRect(x, y, e.getX()-x, e.getY()-y); break;
+			case LOESCHEN: g.clearRect(x, y, e.getX()-x, e.getY()-y); break;
+			}
+			repaint();
+		}
+		
+		
 	}
 
 }
